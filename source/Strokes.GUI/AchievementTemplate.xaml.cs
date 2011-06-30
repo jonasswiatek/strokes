a2fb49ced51f5eba0a1efe569283b03aa83cfe80
Tim Dams <dams.tim@telenet.be>
2011-06-30 20:16:25 +0200

    New databound achievement template (replaces AchieventGui.xaml/cs)

---
+++ source/Strokes.GUI/AchievementTemplate.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Strokes.GUI
+{
+    /// <summary>
+    /// Interaction logic for AchievementTemplate.xaml
+    /// </summary>
+    public partial class AchievementTemplate : UserControl
+    {
+        public AchievementTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public sealed class BoolToImageUriConverter : IValueConverter
+    {
+
+        public object Convert(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            BitmapImage image = new BitmapImage();
+            if (value != null)
+            {
+                try
+                {
+                    image.BeginInit();
+                    image.CacheOption = BitmapCacheOption.OnLoad;
+                    image.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
+
+                    if ((bool)value)
+                        image.UriSource = new System.Uri("pack://application:,,,/Strokes.GUI;component/480px-symbol_check_svg.png");
+                    else
+                        image.UriSource = new System.Uri("pack://application:,,,/Strokes.GUI;component/Cross_red_3d.png");
+
+                    image.EndInit();
+                }
+                catch
+                {
+                    image = null;
+                }
+            }
+            return image;
+        }
+
+        public object ConvertBack(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
