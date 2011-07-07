972e87ac96969c292f1091dee9158119ea7e8053
Tim Dams <dams.tim@telenet.be>
2011-07-07 14:44:06 +0200

    3 new achievements. Kids will be awake anyminute now  and tonite its cinema time (Pirates of the Caribbean)

---
+++ source/Strokes.BasicAchievements.VSX/Achievements/Funny/GotoAchievement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ICSharpCode.NRefactory.Ast;
+using Strokes.BasicAchievements.NRefactory;
+using Strokes.Core;
+
+namespace Strokes.BasicAchievements.Achievements
+{
+    [AchievementDescription("Going Basic style", AchievementDescription = "Please don't use goto anymore. You've got the achievement, happy now?", AchievementCategory = "Funny Achievements")]
+    public class GotoAchievement : NRefactoryAchievement
+    {
+        protected override AbstractAchievementVisitor CreateVisitor()
+        {
+            return new Visitor();
+        }
+
+        private class Visitor : AbstractAchievementVisitor
+        {
+
+            public override object VisitGotoStatement(GotoStatement gotoStatement, object data)
+            {
+                IsAchievementUnlocked = true;
+                return base.VisitGotoStatement(gotoStatement, data);
+            }
+            
+        }
+    }
+}
