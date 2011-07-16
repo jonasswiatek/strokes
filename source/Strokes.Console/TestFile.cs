7f6ab73b70df4603874a1e59f006900df8115767
Claus Jørgensen <10229@iha.dk>
2011-07-16 12:17:06 +0200

    Failed Merge

--- source/Strokes.Console/TestFile.cs
+++ source/Strokes.Console/TestFile.cs
@@ -7,8 +7,6 @@ namespace Strokes.Console
 {
     public class TestFile
     {
-        public string bla { get; set; }
-        public string blaa { get; private set; }
          public TestFile()
          { }
          
--- source/Strokes.GUI/Views/AchievementNotificationBox.xaml.cs
+++ source/Strokes.GUI/Views/AchievementNotificationBox.xaml.cs
@@ -4,10 +4,8 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 using Strokes.Core;
 using Strokes.Core.Model;
-using Strokes.GUI.Resources;
 
 namespace Strokes.GUI.Views
 {
@@ -16,64 +14,20 @@ namespace Strokes.GUI.Views
     /// </summary>
     public partial class AchievementNotificationBox : Window
     {
-        private bool _isEventsBound;
         public AchievementNotificationBox()
         {
             InitializeComponent();
 
             this.CurrentAchievements = new ObservableCollection<AchievementDescriptor>();
             this.DataContext = CurrentAchievements;
-            UnlockedAchievementsList.LayoutUpdated += new EventHandler(UnlockedAchievementsList_LayoutUpdated);
+
+            AchievementContext.AchievementDetectionStarting += AchievementContext_AchievementDetectionStarting;
         }
 
         protected ObservableCollection<AchievementDescriptor> CurrentAchievements
         {
             get;
             set;
-        }
-
-        void UnlockedAchievementsList_LayoutUpdated(object sender, EventArgs e)
-        {
-            if (_isEventsBound)
-                return;
-
-            _isEventsBound = true;
-
-            foreach (AchievementDescriptor item in UnlockedAchievementsList.Items)
-            {
-                var dataItem = item;
-                var gotoCodebutton = FindItemControl(UnlockedAchievementsList, "GotoSource", item) as Image;
-                if(gotoCodebutton == null)
-                    continue;
-
-                if(dataItem.CodeLocaton == null)
-                {
-                    gotoCodebutton.IsEnabled = false;
-                }
-                else
-                {
-                    gotoCodebutton.MouseDown += (se, args) =>
-                    {
-                        var achevementDescriptor = dataItem;
-
-                        AchievementContext.OnAchievementClicked(gotoCodebutton, new AchievementClickedEventArgs
-                        {
-                            AchievementDescriptor = achevementDescriptor,
-                            UIElement = new AchievementViewportControl()
-                            {
-                                DataContext = achevementDescriptor
-                            }
-                        });
-                    };
-                }
-            }
-        }
-
-        private static object FindItemControl(ItemsControl itemsControl, string controlName, object item)
-        {
-            var container = itemsControl.ItemContainerGenerator.ContainerFromItem(item) as ContentPresenter;
-            container.ApplyTemplate();
-            return container.ContentTemplate.FindName(controlName, container);
         }
 
         private void AchievementContext_AchievementDetectionStarting(object sender, System.EventArgs e)
@@ -100,33 +54,37 @@ namespace Strokes.GUI.Views
             {
                 return;
             }
-            
+
             var instance = new AchievementNotificationBox();
             instance.AddAchievements(achievementDescriptors);
 
-            // This is only to support the Strokes.Console-project
             if (Application.Current != null)
             {
-                // During a real Visual Studio integrated run, this is called.
                 instance.Show();
 
-                const int rightMargin = 5;
-                const int bottomMargin = 5;
+                const int rightMargin = 1;
+                const int bottomMargin = 8;
 
-                if (Application.Current.MainWindow.WindowState == WindowState.Normal && Application.Current.MainWindow != instance)
+                if (Application.Current != null &&
+                    Application.Current.MainWindow.WindowState == WindowState.Normal &&
+                    Application.Current.MainWindow != instance)
                 {
-                    instance.Left = Application.Current.MainWindow.Left + Application.Current.MainWindow.Width - instance.Width - rightMargin;
-                    instance.Top = Application.Current.MainWindow.Top + Application.Current.MainWindow.Height - instance.Height - bottomMargin;
+                    instance.Left = Application.Current.MainWindow.Left +
+                                    Application.Current.MainWindow.Width - instance.Width - rightMargin;
+
+                    instance.Top = Application.Current.MainWindow.Top +
+                                   Application.Current.MainWindow.Height - instance.Height - bottomMargin;
                 }
                 else
                 {
                     instance.Left = SystemParameters.PrimaryScreenWidth - instance.Width - rightMargin;
-                    instance.Top = SystemParameters.MaximizedPrimaryScreenHeight - SystemParameters.ResizeFrameHorizontalBorderHeight - instance.Height - bottomMargin;
+
+                    instance.Top = SystemParameters.MaximizedPrimaryScreenHeight -
+                                   SystemParameters.ResizeFrameHorizontalBorderHeight - instance.Height - bottomMargin;
                 }
             }
             else
             {
-                // When activated from a console-app, this is called.
                 new Application().Run(instance);
             }
         }
