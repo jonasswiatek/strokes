1d6a7e005971aa385e0ef51558dfa8fccc0168ee
Claus Jørgensen <10229@iha.dk>
2011-07-16 21:27:33 +0200

    Added close button to the viewportcontrol, and uniformed the background of the viewport and notificationbox to comfort to the dark theme

--- source/Strokes.GUI/Views/AchievementViewportControl.xaml.cs
+++ source/Strokes.GUI/Views/AchievementViewportControl.xaml.cs
@@ -1,10 +1,17 @@
-﻿namespace Strokes.GUI.Resources
+﻿using System.Windows;
+
+namespace Strokes.GUI.Resources
 {
     public partial class AchievementViewportControl
     {
         public AchievementViewportControl()
         {
             InitializeComponent();
+        }
+
+        private void CloseWindowImage_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.Visibility = Visibility.Collapsed;
         }
     }
 }
