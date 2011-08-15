da6c6bb18d40f2ff660d1cefaf46e4d7234480bb
Tim Dams <dams.tim@telenet.be>
2011-08-15 15:46:40 +0200

    Some new achievements on Convert and Array lib usage.

---
+++ source/Strokes.BasicAchievements.VSX/Achievements/MethodCalls/FundamentaldotNetMethods/ArrayClassMethodsAchievement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Strokes.Core;
+
+namespace Strokes.BasicAchievements.Achievements.MethodCalls
+{
+    [AchievementDescription("Clear an array", AchievementDescription = "Use the Array.Clear method to Clear an array", AchievementCategory = "Arrays")]
+    public class ArrayClearMethodAchievement : AbstractMethodCall
+    {
+        public ArrayClearMethodAchievement() : base("System.Array.Clear")
+        {
+        }
+    }
+
+    [AchievementDescription("IndexOf an array", AchievementDescription = "Use the Array.IndexOf method to IndexOf an array", AchievementCategory = "Arrays")]
+    public class ArrayIndexOfMethodAchievement : AbstractMethodCall
+    {
+        public ArrayIndexOfMethodAchievement()
+            : base("System.Array.IndexOf")
+        {
+        }
+    }
+
+    [AchievementDescription("Reverse an array", AchievementDescription = "Use the Array.Reverse method to Reverse an array", AchievementCategory = "Arrays")]
+    public class ArrayReverseMethodAchievement : AbstractMethodCall
+    {
+        public ArrayReverseMethodAchievement()
+            : base("System.Array.Reverse")
+        {
+        }
+    }
+
+    [AchievementDescription("Sort an array", AchievementDescription = "Use the Array.Sort method to sort an array", AchievementCategory = "Arrays")]
+    public class ArraySortMethodAchievement : AbstractMethodCall
+    {
+        public ArraySortMethodAchievement()
+            : base("System.Array.Sort")
+        {
+        }
+    }
+}
---
+++ source/Strokes.BasicAchievements.VSX/Achievements/MethodCalls/FundamentaldotNetMethods/StringMethodsAchievement.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Strokes.Core;
+
+namespace Strokes.BasicAchievements.Achievements.MethodCalls
+{
+    [AchievementDescription("String comparing", AchievementDescription = "Use the String.Compare method", AchievementCategory = "Strings")]
+    public class StringCompareAchievement : AbstractMethodCall
+    {
+        public StringCompareAchievement() : base("System.String.Compare")
+        {
+        }
+    }
+
+    [AchievementDescription("String concaten-what?", AchievementDescription = "Use the String.Concat method", AchievementCategory = "Strings")]
+    public class StringConcatAchievement : AbstractMethodCall
+    {
+        public StringConcatAchievement()
+            : base("System.String.Concat")
+        {
+        }
+    }
+
+    [AchievementDescription("String copying", AchievementDescription = "Use the String.Copy method", AchievementCategory = "Strings")]
+    public class StringCopyAchievement : AbstractMethodCall
+    {
+        public StringCopyAchievement()
+            : base("System.String.Copy")
+        {
+        }
+    }
+
+    [AchievementDescription("String equals", AchievementDescription = "Use the String.Equals method", AchievementCategory = "Strings")]
+    public class StringEqualsAchievement : AbstractMethodCall
+    {
+        public StringEqualsAchievement()
+            : base("System.String.Equals")
+        {
+        }
+    }
+
+    [AchievementDescription("String equals", AchievementDescription = "Use the String.Equals method", AchievementCategory = "Strings")]
+    public class StringEqualsAchievement : AbstractMethodCall
+    {
+        public StringEqualsAchievement()
+            : base("System.String.Equals")
+        {
+        }
+    }
+}
