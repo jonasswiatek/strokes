54304bdeeee4a664478b55aad3340e17a598d330
Tim Dams <dams.tim@telenet.be>
2011-08-16 13:07:43 +0200

    Couple new achievements: array achievements, escape chars and arithmetic operators (and one extremely hard to get funny achievement :p)

---
+++ source/Strokes.BasicAchievements.VSX/Achievements/Arrays/MultipleDimensionArrayDeclarationAchievement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ICSharpCode.NRefactory.Ast;
+using ICSharpCode.NRefactory.Visitors;
+using Strokes.BasicAchievements.NRefactory;
+using Strokes.Core;
+
+namespace Strokes.BasicAchievements.Achievements
+{
+    [AchievementDescription("Declare a multidimensional array", AchievementDescription = "Declare a multidimensional array", AchievementCategory = "Arrays")]
+    public class DeclareMultipleDimArrayAchievement : NRefactoryAchievement
+    {
+        protected override AbstractAchievementVisitor CreateVisitor()
+        {
+            return new Visitor();
+        }
+
+        private class Visitor : AbstractAchievementVisitor
+        {
+            public override object VisitLocalVariableDeclaration(LocalVariableDeclaration localVariableDeclaration, object data)
+            {
+                if (localVariableDeclaration.TypeReference.IsArrayType && localVariableDeclaration.TypeReference.RankSpecifier[0]>1) //Tim= not so happy about hardocing this RankSpecifier, not sure when this specifier contains more than 1 element
+                    UnlockWith(localVariableDeclaration);
+                return base.VisitLocalVariableDeclaration(localVariableDeclaration, data);
+            }
+        }
+    }
+}
---
+++ source/Strokes.BasicAchievements.VSX/Achievements/MethodCalls/Console/PrintWithEscapeCharactersAchievement.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Strokes.Core;
+
+namespace Strokes.BasicAchievements.Achievements.MethodCalls
+{
+    [AchievementDescription("Print a newline using \\n", AchievementDescription = "Print a newline to the console using \\n", AchievementCategory = "Console")]
+    public class PrintNewLineAchievement : AbstractMethodCall
+    {
+        public PrintNewLineAchievement() : base("System.Console.WriteLine")
+        {
+            var requirementSet = new TypeAndValueRequirementSet
+                                     {
+                                         Repeating = true,
+                                         Requirements = new List<TypeAndValueRequirement>
+                                                            {
+                                                                new TypeAndValueRequirement
+                                                                    {
+                                                                        Type = typeof (string),
+                                                                        Regex = @"\n"
+                                                                    }
+                                                            }
+                                     };
+
+            RequiredOverloads.Add(requirementSet);
+        }
+    }
+
+    [AchievementDescription("Print a tab using \\t", AchievementDescription = "Print a horizontal tab to the console using \\t", AchievementCategory = "Console")]
+    public class PrintHorizontalTabAchievement : AbstractMethodCall
+    {
+        public PrintHorizontalTabAchievement()
+            : base("System.Console.WriteLine")
+        {
+            var requirementSet = new TypeAndValueRequirementSet
+            {
+                Repeating = true,
+                Requirements = new List<TypeAndValueRequirement>
+                                                            {
+                                                                new TypeAndValueRequirement
+                                                                    {
+                                                                        Type = typeof (string),
+                                                                        Regex = @"\t"
+                                                                    }
+                                                            }
+            };
+
+            RequiredOverloads.Add(requirementSet);
+        }
+    }
+
+    [AchievementDescription("Print a \" using \\\"", AchievementDescription = "Print a quote (\") to the console using \\\"", AchievementCategory = "Console")]
+    public class PrintQuoteCharAchievement : AbstractMethodCall
+    {
+        public PrintQuoteCharAchievement()
+            : base("System.Console.WriteLine")
+        {
+            var requirementSet = new TypeAndValueRequirementSet
+            {
+                Repeating = true,
+                Requirements = new List<TypeAndValueRequirement>
+                                                            {
+                                                                new TypeAndValueRequirement
+                                                                    {
+                                                                        Type = typeof (string),
+                                                                        Regex = "\\\""
+                                                                    }
+                                                            }
+            };
+
+            RequiredOverloads.Add(requirementSet);
+        }
+    }
+
+    [AchievementDescription("Print a \' using \\\'", AchievementDescription = "Print a quote (\') to the console using \\\'", AchievementCategory = "Console")]
+    public class PrintSingleQuoteCharAchievement : AbstractMethodCall
+    {
+        public PrintSingleQuoteCharAchievement()
+            : base("System.Console.WriteLine")
+        {
+            var requirementSet = new TypeAndValueRequirementSet
+            {
+                Repeating = true,
+                Requirements = new List<TypeAndValueRequirement>
+                                                            {
+                                                                new TypeAndValueRequirement
+                                                                    {
+                                                                        Type = typeof (string),
+                                                                        Regex = "\\\'"
+                                                                    }
+                                                            }
+            };
+
+            RequiredOverloads.Add(requirementSet);
+        }
+    }
+
+    [AchievementDescription("Print a \\ using \\\\", AchievementDescription = "Print a quote (\\) to the console using \\\\", AchievementCategory = "Console")]
+    public class PrintBackSlashCharAchievement : AbstractMethodCall
+    {
+        public PrintBackSlashCharAchievement()
+            : base("System.Console.WriteLine")
+        {
+            var requirementSet = new TypeAndValueRequirementSet
+            {
+                Repeating = true,
+                Requirements = new List<TypeAndValueRequirement>
+                                                            {
+                                                                new TypeAndValueRequirement
+                                                                    {
+                                                                        Type = typeof (string),
+                                                                        Regex = "\\\\"
+                                                                    }
+                                                            }
+            };
+
+            RequiredOverloads.Add(requirementSet);
+        }
+    }
+
+}
