39f343b4816b0b79dd15a86cd2446eeff5683956
Tim Dams <dams.tim@telenet.be>
2011-08-17 10:37:56 +0200

    4 new funny achievements: -     * Telling a story – Created a method longer then 100 lines. -     * Epic tale – Created a method longer then 300 lines. -     * Magic-maker – 5 or more magic numbers in a method -     * The Parametizer: Created a method with more than 10 non optional parameters

--- source/Strokes.BasicAchievements.VSX/Achievements/Funny/SomeFunnyAchievement.cs
+++ source/Strokes.BasicAchievements.VSX/Achievements/Funny/SomeFunnyAchievement.cs
@@ -18,15 +18,82 @@ namespace Strokes.BasicAchievements.Achievements
      * The Numerologist: Name all your controls: control1, control2, control3, etc
      * The Compound Sentence – created a method name containing one or more of the words: for, and, nor, but, or, yet, so.
      * Overloaded – Create a method with more that 10 overloads
-     * Telling a story – Created a method longer then 100 lines.
-     * Epic tale – Created a method longer then 300 lines.
      * Magic-maker – 5 or more magic numbers in a method
-     * The Parametizer: Created a method with more than 10 non optional parameters
      * "Over-qualified": "using System;" is your only using directive.
      * "Under-qualified": Write a using directive for every conceivable namespace used by your program.
      * e.e. cummings":  Write so many nested classes, functions, and control structures, that the first character of a line is on or after column 73. 
      * 
      * */
+
+
+    [AchievementDescription("Telling a story", AchievementDescription = "Created a method longer than 100 lines", AchievementCategory = "Funny")]
+    public class EpicTaleAchievement : NRefactoryAchievement
+    {
+        protected override AbstractAchievementVisitor CreateVisitor()
+        {
+            return new Visitor();
+        }
+
+        private class Visitor : AbstractAchievementVisitor
+        {
+            int count = 0;
+            public override object VisitMethodDeclaration(MethodDeclaration methodDeclaration, object data)
+            {
+                if (methodDeclaration.Body.EndLocation.Line - methodDeclaration.Body.StartLocation.Line >= 100)
+                    UnlockWith(methodDeclaration);
+                return base.VisitMethodDeclaration(methodDeclaration, data);
+            }
+        }
+    }
+
+    [AchievementDescription("Epic Tale", AchievementDescription = "Created a method longer than 300 lines", AchievementCategory = "Funny")]
+    public class EpicTaleAchievement : NRefactoryAchievement
+    {
+        protected override AbstractAchievementVisitor CreateVisitor()
+        {
+            return new Visitor();
+        }
+
+        private class Visitor : AbstractAchievementVisitor
+        {
+            int count = 0;
+            public override object VisitMethodDeclaration(MethodDeclaration methodDeclaration, object data)
+            {
+                if (methodDeclaration.Body.EndLocation.Line - methodDeclaration.Body.StartLocation.Line >= 300)
+                    UnlockWith(methodDeclaration);
+                return base.VisitMethodDeclaration(methodDeclaration, data);
+            }
+        }
+    }
+
+    [AchievementDescription("The Parametizer", AchievementDescription = "Created a method with more than 10 non optional parameters", AchievementCategory = "Funny")]
+    public class ParametizerAchievement : NRefactoryAchievement
+    {
+        protected override AbstractAchievementVisitor CreateVisitor()
+        {
+            return new Visitor();
+        }
+
+        private class Visitor : AbstractAchievementVisitor
+        {
+            int count = 0;
+            public override object VisitMethodDeclaration(MethodDeclaration methodDeclaration, object data)
+            {
+                if (methodDeclaration.Parameters.Count > 10)
+                {
+                    foreach (var param in methodDeclaration.Parameters)
+                    {
+                        if (param.ParamModifier.HasFlag(ParameterModifiers.Optional))
+                            count++;
+
+                    }
+                    if (count > 10)
+                        UnlockWith(methodDeclaration);
+                }
+                return base.VisitMethodDeclaration(methodDeclaration, data);
+            }
+        }
+    }
 
     [AchievementDescription("Grand Unified Theory", AchievementDescription = "Design a class that implements at least 5 interfaces", AchievementCategory = "Funny")]
     public class GUTAchievement : NRefactoryAchievement
@@ -98,15 +165,15 @@ namespace Strokes.BasicAchievements.Achievements
             int count = 0;
             public override object VisitMethodDeclaration(MethodDeclaration methodDeclaration, object data)
             {
-                if (methodDeclaration.Parameters.Count > 2)
+                if (methodDeclaration.Parameters.Count > 9)
                 {
                     foreach (var param in methodDeclaration.Parameters)
                     {
-                        if (param.ParamModifier == ParameterModifiers.Out)
+                        if (param.ParamModifier.HasFlag(ParameterModifiers.Out))
                             count++;
 
                     }
-                    if (count > 2)
+                    if (count > 9)
                         UnlockWith(methodDeclaration);
                 }
                 return base.VisitMethodDeclaration(methodDeclaration, data);
