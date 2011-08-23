e970aa3ad1a99a25f380152b5107c090536f760a
Jonas Swiatek <jonas.swiatek@gmail.com>
2011-08-23 22:25:39 +0200

    And the files for the last checkin

---
+++ source/Strokes.ChallengeTemplate/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Strokes.Challenges.Student.CalculatorChallenge;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+
+namespace $safeprojectname$
+{
+	public class Calculator : ICalculator
+	{
+        public int Add(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Subtract(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Multiply(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public float Divide(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+	}
+}
