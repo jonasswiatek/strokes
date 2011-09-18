844ec894323f4d2519734a5474f7ac9e819b39d9
Jonas Swiatek <jonas.swiatek@gmail.com>
2011-09-18 22:17:50 +0200

    Merge fail

---
+++ source/Strokes.BasicAchievements.VSX/Achievements/MethodCalls/MethodBasics/CreateMethodReturnTypeX.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ICSharpCode.NRefactory.Ast;
+using Strokes.BasicAchievements.NRefactory;
+using Strokes.Core;
+
+namespace Strokes.BasicAchievements.Achievements
+{
+    [AchievementDescription("Into the void", AchievementDescription = "Create a method that returns void", AchievementCategory = "Method")]
+    public class CreateMethodReturnVoidAchievement : NRefactoryAchievement
+    {
+        protected override AbstractAchievementVisitor CreateVisitor()
+        {
+            return new Visitor();
+        }
+
+        private class Visitor : AbstractAchievementVisitor
+        {
+            public override object VisitMethodDeclaration(MethodDeclaration methodDeclaration, object data)
+            {
+                if (!methodDeclaration.Name.ToLower().Equals("main") && !methodDeclaration.Modifier.HasFlag(Modifiers.Constructors))
+                {
+                    if (!methodDeclaration.IsExtensionMethod && !methodDeclaration.Modifier.HasFlag(Modifiers.Abstract))
+                    {
+                        if (methodDeclaration.TypeReference.ToString().Equals("System.Void"))
+                            UnlockWith(methodDeclaration);
+                    }
+                }
+                return base.VisitMethodDeclaration(methodDeclaration, data);
+            }
+        }
+    }
+
+    [AchievementDescription("Return int", AchievementDescription = "Create a method that returns an int", AchievementCategory = "Method")]
+    public class CreateMethodReturnIntAchievement : NRefactoryAchievement
+    {
+        protected override AbstractAchievementVisitor CreateVisitor()
+        {
+            return new Visitor();
+        }
+
+        private class Visitor : AbstractAchievementVisitor
+        {
+            public override object VisitMethodDeclaration(MethodDeclaration methodDeclaration, object data)
+            {
+                if (!methodDeclaration.Name.ToLower().Equals("main") && !methodDeclaration.Modifier.HasFlag(Modifiers.Constructors))
+                {
+                    if (!methodDeclaration.IsExtensionMethod && !methodDeclaration.Modifier.HasFlag(Modifiers.Abstract))
+                    {
+                        if (methodDeclaration.TypeReference.ToString().Equals("System.Int32"))
+                            UnlockWith(methodDeclaration);
+                    }
+                }
+                return base.VisitMethodDeclaration(methodDeclaration, data);
+            }
+        }
+    }
+
+    [AchievementDescription("Return a string", AchievementDescription = "Create a method that returns a string", AchievementCategory = "Method")]
+    public class CreateMethodReturnStringAchievement : NRefactoryAchievement
+    {
+        protected override AbstractAchievementVisitor CreateVisitor()
+        {
+            return new Visitor();
+        }
+
+        private class Visitor : AbstractAchievementVisitor
+        {
+            public override object VisitMethodDeclaration(MethodDeclaration methodDeclaration, object data)
+            {
+                if (!methodDeclaration.Name.ToLower().Equals("main") && !methodDeclaration.Modifier.HasFlag(Modifiers.Constructors))
+                {
+                    if (!methodDeclaration.IsExtensionMethod && !methodDeclaration.Modifier.HasFlag(Modifiers.Abstract))
+                    {
+                        if (methodDeclaration.TypeReference.ToString().Equals("System.String"))
+                            UnlockWith(methodDeclaration);
+                    }
+                }
+                return base.VisitMethodDeclaration(methodDeclaration, data);
+            }
+        }
+    }
+
+    [AchievementDescription("Return a bool", AchievementDescription = "Create a method that returns a boolean", AchievementCategory = "Method")]
+    public class CreateMethodReturnBoolAchievement : NRefactoryAchievement
+    {
+        protected override AbstractAchievementVisitor CreateVisitor()
+        {
+            return new Visitor();
+        }
+
+        private class Visitor : AbstractAchievementVisitor
+        {
+            public override object VisitMethodDeclaration(MethodDeclaration methodDeclaration, object data)
+            {
+                if (!methodDeclaration.Name.ToLower().Equals("main") && !methodDeclaration.Modifier.HasFlag(Modifiers.Constructors))
+                {
+                    if (!methodDeclaration.IsExtensionMethod && !methodDeclaration.Modifier.HasFlag(Modifiers.Abstract))
+                    {
+                        if (methodDeclaration.TypeReference.ToString().Equals("System.Boolean"))
+                            UnlockWith(methodDeclaration);
+                    }
+                }
+                return base.VisitMethodDeclaration(methodDeclaration, data);
+            }
+        }
+    }
+
+    [AchievementDescription("Return char", AchievementDescription = "Create a method that returns a char", AchievementCategory = "Method")]
+    public class CreateMethodReturnCharAchievement : NRefactoryAchievement
+    {
+        protected override AbstractAchievementVisitor CreateVisitor()
+        {
+            return new Visitor();
+        }
+
+        private class Visitor : AbstractAchievementVisitor
+        {
+            public override object VisitMethodDeclaration(MethodDeclaration methodDeclaration, object data)
+            {
+                if (!methodDeclaration.Name.ToLower().Equals("main") && !methodDeclaration.Modifier.HasFlag(Modifiers.Constructors))
+                {
+                    if (!methodDeclaration.IsExtensionMethod && !methodDeclaration.Modifier.HasFlag(Modifiers.Abstract))
+                    {
+                        if (methodDeclaration.TypeReference.ToString().Equals("System.Char"))
+                            UnlockWith(methodDeclaration);
+                    }
+                }
+                return base.VisitMethodDeclaration(methodDeclaration, data);
+            }
+        }
+    }
+
+
+    [AchievementDescription("Return double", AchievementDescription = "Create a method that returns a double", AchievementCategory = "Method")]
+    public class CreateMethodReturnDoubleAchievement : NRefactoryAchievement
+    {
+        protected override AbstractAchievementVisitor CreateVisitor()
+        {
+            return new Visitor();
+        }
+
+        private class Visitor : AbstractAchievementVisitor
+        {
+            public override object VisitMethodDeclaration(MethodDeclaration methodDeclaration, object data)
+            {
+                if (!methodDeclaration.Name.ToLower().Equals("main") && !methodDeclaration.Modifier.HasFlag(Modifiers.Constructors))
+                {
+                    if (!methodDeclaration.IsExtensionMethod && !methodDeclaration.Modifier.HasFlag(Modifiers.Abstract))
+                    {
+                        if (methodDeclaration.TypeReference.ToString().Equals("System.Double"))
+                            UnlockWith(methodDeclaration);
+                    }
+                }
+                return base.VisitMethodDeclaration(methodDeclaration, data);
+            }
+        }
+    }
+
+    [AchievementDescription("Return float", AchievementDescription = "Create a method that returns a float", AchievementCategory = "Method")]
+    public class CreateMethodReturnFloatAchievement : NRefactoryAchievement
+    {
+        protected override AbstractAchievementVisitor CreateVisitor()
+        {
+            return new Visitor();
+        }
+
+        private class Visitor : AbstractAchievementVisitor
+        {
+            public override object VisitMethodDeclaration(MethodDeclaration methodDeclaration, object data)
+            {
+                if (!methodDeclaration.Name.ToLower().Equals("main") && !methodDeclaration.Modifier.HasFlag(Modifiers.Constructors))
+                {
+                    if (!methodDeclaration.IsExtensionMethod && !methodDeclaration.Modifier.HasFlag(Modifiers.Abstract))
+                    {
+                        if (methodDeclaration.TypeReference.ToString().Equals("System.Single"))
+                            UnlockWith(methodDeclaration);
+                    }
+                }
+                return base.VisitMethodDeclaration(methodDeclaration, data);
+            }
+        }
+    }
+
+}
---
+++ source/Strokes.BasicAchievements.VSX/Achievements/ProgramFlow/IfShorthandAchievement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ICSharpCode.NRefactory.Ast;
+using Strokes.BasicAchievements.NRefactory;
+using Strokes.Core;
+
+namespace Strokes.BasicAchievements.Achievements
+{
+    [AchievementDescription("?:", AchievementDescription = "Write a conditional expression",
+        AchievementCategory = "Program flow")]
+    public class IfShorthandAchievement : NRefactoryAchievement
+    {
+        protected override AbstractAchievementVisitor CreateVisitor()
+        {
+            return new Visitor();
+        }
+
+        private class Visitor : AbstractAchievementVisitor
+        {
+
+            public override object VisitConditionalExpression(ConditionalExpression conditionalExpression, object data)
+            {
+                UnlockWith(conditionalExpression);
+                return base.VisitConditionalExpression(conditionalExpression, data);
+            }
+        }
+    }
+}
