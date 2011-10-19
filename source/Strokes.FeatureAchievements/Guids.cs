77eb5edb04be9d289ce03e4c5f7ed607f918ee57
Jonas Swiatek <jonas.swiatek@gmail.com>
2011-10-20 00:05:43 +0200

    Added feature achievements project. This is a VS Package Project because it needs to integrate tightly with the Visual Studio IDE

---
+++ source/Strokes.FeatureAchievements/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace Codecube.Strokes_FeatureAchievements
+{
+    static class GuidList
+    {
+        public const string guidStrokes_FeatureAchievementsPkgString = "332b68ad-b16a-47c7-ac8b-a8d30aade04e";
+        public const string guidStrokes_FeatureAchievementsCmdSetString = "5d0265ed-d35f-424a-957f-e6d860e1e14a";
+
+        public static readonly Guid guidStrokes_FeatureAchievementsCmdSet = new Guid(guidStrokes_FeatureAchievementsCmdSetString);
+    };
+}
---
+++ source/Strokes.FeatureAchievements/Strokes.FeatureAchievementsPackage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.ComponentModel.Design;
+using Microsoft.Win32;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using Strokes.Core.Integration;
+
+namespace Codecube.Strokes_FeatureAchievements
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    [Guid(GuidList.guidStrokes_FeatureAchievementsPkgString)]
+    public sealed class FeatureAchievementsPackage : Package
+    {
+        #region Package Members
+        protected override void Initialize()
+        {
+            base.Initialize();
+            var als = GetService<IAchevementLibraryService>();
+            als.RegisterAchievementAssembly(GetType().Assembly);
+        }
+        #endregion
+
+        #region Tool methods
+        public T GetService<T>()
+        {
+            return (T)GetService(typeof(T));
+        }
+        #endregion
+    }
+}
