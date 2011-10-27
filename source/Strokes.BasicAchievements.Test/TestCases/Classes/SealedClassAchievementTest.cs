79a38b7472babd935d2b2dec7e6d3f6f9761caab
Jonas Swiatek <jonas.swiatek@gmail.com>
2011-10-28 00:09:14 +0200

    Added unit tests for the two new achievements

---
+++ source/Strokes.BasicAchievements.Test/TestCases/Classes/SealedClassAchievementTest.cs
@@ -0,0 +1,9 @@
+﻿using Strokes.BasicAchievements.Achievements;
+
+namespace Strokes.BasicAchievements.Test.TestCases.Classes
+{
+    [ExpectUnlock(typeof(SealedClassAchievement))]
+    public sealed class SealedClassAchievementTest
+    {
+    }
+}
